b7ce6fb62b781bb0ff7be1d1b7e96d66a6bccb76
Jane Choi <55301593+jaeyoung-jane-choi@users.noreply.github.com>
2021-05-18 15:31:27 +0900

    Update [17] Letter Combinations of a Phone Number.py

--- May/[17] Letter Combinations of a Phone Number.py
+++ May/[17] Letter Combinations of a Phone Number.py
@@ -5,6 +5,7 @@ def letterCombinations(digits):
     :type digits: str
     :rtype: List[str]
     """
+    if len(digits) == 0 : return []
     dic = {2: 'abc', 3 :'def', 4: 'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'}
 
     res = ['']
